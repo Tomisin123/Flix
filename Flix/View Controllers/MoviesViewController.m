43060643b4fbbba47195b8a5188e90ff143599a4
Tomisin Ogunfunmi <tomisin@fb.com>
2021-06-23 16:05:38 -0500

    Added Pull to Refresh feature

--- Flix/View Controllers/MoviesViewController.m
+++ Flix/View Controllers/MoviesViewController.m
@@ -13,6 +13,7 @@
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) NSArray *movies;
+@property (nonatomic, strong) UIRefreshControl *refreshControl;
 
 @end
 
@@ -23,8 +24,15 @@
     
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
+    [self fetchMovies];
     
-    // Do any additional setup after loading the view.
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    [self.refreshControl addTarget:self action:@selector(fetchMovies) forControlEvents:UIControlEventValueChanged];
+    [self.tableView insertSubview: self.refreshControl atIndex:0];
+    //[self.tableView addSubview:self.refreshControl];
+}
+
+- (void) fetchMovies {
     NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
@@ -46,6 +54,7 @@
                //Reload your table view data, bc movies is slow to load/ may have changed
                [self.tableView reloadData];
            }
+        [self.refreshControl endRefreshing];
        }];
     [task resume];
 }
